bf10d445a5646ad781e2aac34ec88b9f3f18eaed
adriannebulao <amcbulao@gmail.com>
2024-10-09 13:27:06 +0800

    feat: create products screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,26 +2,17 @@ import 'package:cs3midlogin/screens/dashboard.dart';
 import 'package:cs3midlogin/screens/home_screen.dart';
 import 'package:cs3midlogin/screens/login_screen.dart';
 import 'package:cs3midlogin/screens/sign_up_screen.dart';
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MainApp());
+  runApp(const MainApp());
 }
 
 class MainApp extends StatelessWidget {
-  MainApp({super.key});
-
-  final dio = Dio();
-
-  void getHttp() async {
-    final response = await dio.get('https://fakestoreapi.com/products');
-    print(response.data[0]);
-  }
+  const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    getHttp();
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       initialRoute: '/',
