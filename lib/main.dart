6ba36bc1039285af87d689db223cdf5ca077a843
adriannebulao <amcbulao@gmail.com>
2024-10-09 13:14:43 +0800

    feat: add dio dependency

--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,26 @@ import 'package:cs3midlogin/screens/dashboard.dart';
 import 'package:cs3midlogin/screens/home_screen.dart';
 import 'package:cs3midlogin/screens/login_screen.dart';
 import 'package:cs3midlogin/screens/sign_up_screen.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MainApp());
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  MainApp({super.key});
+
+  final dio = Dio();
+
+  void getHttp() async {
+    final response = await dio.get('https://fakestoreapi.com/products');
+    print(response.data[0]);
+  }
 
   @override
   Widget build(BuildContext context) {
+    getHttp();
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       initialRoute: '/',
